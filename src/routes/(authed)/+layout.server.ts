087d4f3724d08fdedd5d95485b6be8f6cca1d306
Jian Yuan Lee <jianyuan@gmail.com>
2024-10-27 23:50:33 +0000

    ref: protect guest-only and user-only routes

---
+++ src/routes/(authed)/+layout.server.ts
@@ -0,0 +1,8 @@
+import { redirect } from '@sveltejs/kit'
+import type { LayoutServerLoad } from './$types'
+
+export const load: LayoutServerLoad = async ({ locals }) => {
+  if (!locals.user) {
+    redirect(303, '/login')
+  }
+}
